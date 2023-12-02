1005cee482563d3cfaef021fc94ade1fd7a54836
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2023-12-02 12:45:24 -0300

    commit portfolio

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+// Ao clicar em Exercicios em JS abri uma janela do repositório dos exercicios
+function abrirJanela(){
+  window.open('https://github.com/jeffmmartins/Exercicios-LogicadaProgramacao-com-JavaScript')
+}
+
+ // Ao passar o mouse no campo sobre mim , alterar a cor de fundo 
+function modifica(corFundo) {
+  document.getElementById('fonte').style.backgroundColor = corFundo
+
+}
+
+
+
