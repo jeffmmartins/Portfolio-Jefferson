963903bf0b802aadfc64b0be8f218afa0e32bdf1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-04-14 19:50:08 -0300

    Commit update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
 
 // Ao clicar em Exercicios em JS abri uma janela do repositório dos exercicios
-function abrirJanela(){
-  window.open('https://github.com/jeffmmartins/Exercicios-LogicadaProgramacao-com-JavaScript')
+function abrirJanela() {
+  window.open(
+    "https://github.com/jeffmmartins/Exercicios-LogicadaProgramacao-com-JavaScript"
+  );
 }
+
+
 
 
 
